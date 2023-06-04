54b8e54a1541d427465974b7aee244e501feaa1b
Huykhtn <ngocvu1119@gmail.com>
2023-06-04 19:44:50 +0700

    update Context

---
+++ practice_usecontext/ContextTS/src/App.tsx
@@ -0,0 +1,18 @@
+
+
+import { CounterProvider } from './context/CounterContext';
+import Counter from './Counter';
+
+function App() {
+  
+
+  return (
+    <>
+        <CounterProvider>
+        <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+      </CounterProvider>
+    </>
+  )
+}
+
+export default App
